1171e68a4faa115bf89e94e12a5225332f07e112
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-11-28 22:34:45 +0700

    feat: poster card component

--- src/components/common/poster-card/index.tsx
+++ src/components/common/poster-card/index.tsx
@@ -1,21 +1,18 @@
+import React from "react";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { tmdbBaseImageUrl, tmdbImageSize } from "@/constants/movie";
+import { IMovie } from "@/interfaces/movie";
 
 import imdbLogo from "@/assets/imdb-logo.png";
-import tomatoLogo from "@/assets/tomato-logo.png";
 import playIcon from "@/assets/play-icon.png";
+import starIcon from "@/assets/star.svg";
 
-interface PosterCard {
-	backdrop_path: string;
-	original_title: string;
-	overview: string;
-	popularity: number;
-	poster_path: string;
-	release_date: string;
-	title: string;
-}
+type PosterCardProps = {
+	poster: IMovie;
+};
 
-export const PosterCard = ({ poster }: { poster: PosterCard }) => {
+export const PosterCard: React.FC<PosterCardProps> = ({ poster }) => {
 	return (
 		<div className="absolute w-full h-full flex justify-center text-white">
 			<div className=" w-full h-full flex items-center absolute  z-10 md:max-w-7xl">
@@ -23,18 +20,27 @@ export const PosterCard = ({ poster }: { poster: PosterCard }) => {
 					<h1 className="text-5xl font-bold">{poster.title}</h1>
 					<div className="flex gap-[34px]">
 						<div className="flex items-center gap-2.5">
-							<img src={imdbLogo} alt="" className="w-[35px] h-[17px]" />
+							<img
+								src={imdbLogo}
+								alt="imdb logo"
+								className="w-[35px] h-[17px]"
+							/>
 							<p className="font-normal text-xs">{poster.popularity}</p>
 						</div>
-						<div className="flex items-center gap-2.5">
-							<img src={tomatoLogo} alt="" className="w-4 h-[17px]" />
-							<p className="font-normal text-xs">97%</p>
+						<div className="flex items-center gap-2.5 bg-brand-grey/900 py-2 px-4 rounded-md bg-opacity-80 backdrop-blur-md">
+							<img src={starIcon} alt="star icon" className="w-4 h-[17px]" />
+							<p className="font-normal text-xs">
+								{poster.vote_average.toFixed(1)}
+							</p>
 						</div>
+						<Badge className="uppercase text-[14px] font-bold py-[6px] w-fit px-4">
+							trending movie
+						</Badge>
 					</div>
 					<p className="font-medium text-[14px]">{poster.overview}</p>
 					<Button className="py-[6px] px-4 bg-brand-rose/700 hover:bg-brand-rose/800 w-[170px]">
 						<div className="flex items-center gap-2">
-							<img src={playIcon} alt="" className="w-4 h-4" />
+							<img src={playIcon} alt="play icon" className="w-4 h-4" />
 							<span className="font-bold text-[14px] uppercase">
 								Watch Trailer
 							</span>
