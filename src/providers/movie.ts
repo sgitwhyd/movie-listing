23b13baa8497591bcf91ae3aac38bc25082b339c
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-11-28 22:33:26 +0700

    feat:  create query to fetch genre, upcoming, trending, detail movie

--- src/providers/movie.ts
+++ src/providers/movie.ts
@@ -1,6 +1,11 @@
 import apiInstance from "@/libs/axios";
 import { useQuery } from "@tanstack/react-query";
-import { IMovieResponse } from "@/interfaces/movie";
+import {
+	IMovieGenreResponse,
+	IMovieResponse,
+	IDetailMovieResponseWithBackdrop,
+} from "@/interfaces/movie";
+import { AxiosResponse } from "axios";
 
 export const useNowPlayingMovie = () => {
 	return useQuery<IMovieResponse>({
@@ -16,3 +21,68 @@ export const useNowPlayingMovie = () => {
 		},
 	});
 };
+
+export const useMovieGenre = () =>
+	useQuery<IMovieGenreResponse>({
+		queryKey: ["fetch movie genres"],
+		queryFn: async () => {
+			const response = await apiInstance.get("/genre/movie/list", {
+				params: {
+					language: "en",
+				},
+			});
+			return response.data;
+		},
+	});
+
+export const useUpcomingMovies = ({ page = 1 }: { page: number }) => {
+	return useQuery<IMovieResponse>({
+		queryKey: ["upcomingMovies", { page }], // Use a unique identifier as the first element of the queryKey
+		queryFn: async () => {
+			try {
+				const response: AxiosResponse<IMovieResponse> = await apiInstance.get(
+					"/movie/upcoming",
+					{
+						params: {
+							language: "en-US",
+							page,
+						},
+					}
+				);
+
+				return response.data;
+			} catch (error) {
+				console.error("Error in useUpcomingMovies:", error);
+				throw error;
+			}
+		},
+	});
+};
+
+export const useTrendingMovies = () =>
+	useQuery<IMovieResponse>({
+		queryKey: ["trending_movies"],
+		queryFn: async () => {
+			const response = await apiInstance.get("/trending/movie/week", {
+				params: {
+					language: "en-US",
+				},
+			});
+			return response.data;
+		},
+	});
+
+export const useDetailMovie = ({ movie_id }: { movie_id: string }) =>
+	useQuery<IDetailMovieResponseWithBackdrop>({
+		queryKey: ["detail_movie", { movie_id }],
+		queryFn: async () => {
+			const requestDetailMovie = await apiInstance.get(`/movie/${movie_id}`);
+			const requestBackgdropMovie = await apiInstance.get(
+				`/movie/${movie_id}/images`
+			);
+			return {
+				movie: requestDetailMovie.data,
+				movie_backdrops: requestBackgdropMovie.data,
+			};
+		},
+	});
