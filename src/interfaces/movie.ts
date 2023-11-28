d61b4b73d102db40440c0b3a875604be7be68b16
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-11-28 22:32:00 +0700

    feat: adding detail, genre, backdrop movie interface

--- src/interfaces/movie.ts
+++ src/interfaces/movie.ts
@@ -25,3 +25,74 @@ export interface IMovieResponse {
 	total_pages: number;
 	total_results: number;
 }
+
+export interface IMovieGenre {
+	id: number;
+	name: string;
+}
+
+export interface IMovieGenreResponse {
+	genres: IMovieGenre[];
+}
+
+export interface IDetailMovieResponse {
+	adult: false;
+	backdrop_path: string;
+	belongs_to_collection: {
+		id: number;
+		name: string;
+		poster_path: string;
+		backdrop_path: string;
+	} | null;
+	budget: 100000000;
+	genres: IMovieGenre[];
+	homepage: string;
+	id: number;
+	imdb_id: string;
+	original_language: string;
+	original_title: string;
+	overview: string;
+	popularity: number;
+	poster_path: string;
+	production_companies: {
+		id: number;
+		logo_path: string;
+		name: string;
+		origin_country: string;
+	}[];
+	production_countries: {
+		iso_3166_1: string;
+		name: string;
+	}[];
+	release_date: string;
+	revenue: number;
+	runtime: number;
+	spoken_languages: {
+		english_name: string;
+		iso_639_1: string;
+		name: string;
+	}[];
+	status: string;
+	tagline: string;
+	title: string;
+	video: BlobEvent;
+	vote_average: number;
+	vote_count: number;
+}
+
+interface IBackdrops {
+	aspect_ratio: number;
+	height: number;
+	iso_639_1: string | null;
+	file_path: string;
+	vote_average: number;
+	vote_count: number;
+	width: number;
+}
+
+export interface IDetailMovieResponseWithBackdrop {
+	movie: IDetailMovieResponse;
+	movie_backdrops: {
+		backdrops: IBackdrops[];
+	};
+}
